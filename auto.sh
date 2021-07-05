947557eab208d27713c1babfcab47d6a35c7c49f
scratchedguitar <31390860+scratchedguitar@users.noreply.github.com>
2021-07-05 11:33:51 +0100

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -2,7 +2,7 @@ apt update
 apt upgrade -y
 apt dist-upgrade -y
 cd ~/
-apt install git -y
+apt install git libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-libav gstreamer1.0-vaapi gstreamer1.0-plugins-bad -y
 git clone https://github.com/FD-/RPiPlay.git
 cd RPiPlay
 apt-get install cmake -y

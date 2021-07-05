dab04bccdd6843a8eca999c383d15dc280a362b8
scratchedguitar <31390860+scratchedguitar@users.noreply.github.com>
2021-07-05 11:00:41 +0100

    Create auto.sh

---
+++ auto.sh
@@ -0,0 +1,16 @@
+cd ~/
+git clone https://github.com/FD-/RPiPlay.git
+cd RPiPlay
+apt-get install cmake -y
+apt-get install libavahi-compat-libdnssd-dev -y
+apt-get install libplist-dev -y
+apt-get install libssl-dev -y
+mkdir build
+cd build
+cmake ..
+make -j
+cd ..
+cp rpiplay.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable rpiplay
+systemctl start rpiplay

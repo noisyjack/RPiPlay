b8e3741d618e40c8d171580b9b41661aac7506e2
scratchedguitar <31390860+scratchedguitar@users.noreply.github.com>
2021-07-05 11:45:04 +0100

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -3,7 +3,7 @@ apt upgrade -y
 apt dist-upgrade -y
 cd ~/
 apt install git libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-libav gstreamer1.0-vaapi gstreamer1.0-plugins-bad -y
-git clone https://github.com/FD-/RPiPlay.git
+git clone https://github.com/scratchedguitar/RPiPlay.git
 cd RPiPlay
 apt-get install cmake -y
 apt-get install libavahi-compat-libdnssd-dev -y

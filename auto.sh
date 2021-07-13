3f69349fe1e74c49a3be9ea61fd7a4309aef81e1
scratchedguitar <31390860+scratchedguitar@users.noreply.github.com>
2021-07-13 20:27:54 +0100

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -4,6 +4,8 @@ apt dist-upgrade -y
 cd ~/
 apt install git -y
 git clone https://github.com/scratchedguitar/RPiPlay.git
+git checkout fixres
+git pull
 cd RPiPlay
 apt-get install cmake -y
 apt-get install libavahi-compat-libdnssd-dev -y
